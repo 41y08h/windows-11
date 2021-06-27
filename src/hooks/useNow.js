fa208f1493439de6f03ca035b4b78dcf79231fef
Piyush <piyush41y08h@gmail.com>
2021-06-27 19:15:16 +0530

    fix useNow hook

--- src/hooks/useNow.js
+++ src/hooks/useNow.js
@@ -1,22 +1,41 @@
 import { useEffect, useState } from "react";
 
+function zerofy(n) {
+  if (n.toString().length > 1) return n;
+
+  return `0${n.toString()}`;
+}
+
 export default function useNow() {
   // eslint-disable-next-line
-  const [_, setDateTime] = useState(new Date());
+  const [number, setNumber] = useState(1);
 
   useEffect(() => {
     const interval = setInterval(() => {
-      setDateTime(new Date());
+      setNumber((old) => old + 1);
     }, 1000 * 60);
 
     return () => {
       clearInterval(interval);
     };
+  }, [number]);
+
+  useEffect(() => {
+    const ms = (60 - new Date().getSeconds()) * 1000;
+    const timeout = setTimeout(() => {
+      setNumber((old) => old + 1);
+    }, ms);
+
+    return () => {
+      clearTimeout(timeout);
+    };
   }, []);
 
   const value = {
-    time: `${new Date().getHours()}:${new Date().getMinutes()}`,
-    date: `${new Date().getDate()}-${new Date().getMonth()}-${new Date().getFullYear()}`,
+    time: `${zerofy(new Date().getHours())}:${zerofy(new Date().getMinutes())}`,
+    date: `${zerofy(new Date().getDate())}-${zerofy(
+      new Date().getMonth()
+    )}-${zerofy(new Date().getFullYear())}`,
   };
 
   return value;
