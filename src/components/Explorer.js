52864c484aef028634d59aed22790fb789a7d3d0
Piyush <piyush41y08h@gmail.com>
2021-06-27 19:25:18 +0530

    make explorer dialog draggable

--- src/components/Explorer.js
+++ src/components/Explorer.js
@@ -1,4 +1,5 @@
 import { ReactComponent as ExplorerIcon } from "../assets/explorer.svg";
+import Draggable from "react-draggable";
 
 function Folder({ name }) {
   return (
@@ -40,52 +41,60 @@ function ExplorerPaneItem({ iconName, text, ...props }) {
 
 export default function Explorer() {
   return (
-    <div
-      className="absolute top-0 left-0 bg-gray-100 rounded-md pb-4"
-      style={{ width: "50rem", height: "30rem" }}
-    >
-      <div className="flex justify-between">
-        <div className="flex items-center space-x-4 pl-4 py-2">
-          <ExplorerIcon style={{ width: "20px", height: "auto" }} />
-          <h2 className="text-sm">File Explorer</h2>
+    <Draggable>
+      <div
+        className="absolute top-20 left-20 bg-gray-100 rounded-md pb-4"
+        style={{
+          width: "50rem",
+          height: "30rem",
+        }}
+      >
+        <div className="flex justify-between">
+          <div className="flex items-center space-x-4 pl-4 py-2">
+            <ExplorerIcon style={{ width: "20px", height: "auto" }} />
+            <h2 className="text-sm">File Explorer</h2>
+          </div>
+          <div className="flex">
+            <button className="p-2 px-3 text-xs hover:bg-gray-50">
+              <i
+                className="ms-Icon ms-Icon--ChromeMinimize"
+                aria-hidden="true"
+              />
+            </button>
+            <button className="p-2 px-3 text-xs hover:bg-gray-50">
+              <i className="ms-Icon ms-Icon--Checkbox" aria-hidden="true" />
+            </button>
+            <button className="p-2 px-3 text-xs hover:bg-gray-50">
+              <i className="ms-Icon ms-Icon--ChromeClose" aria-hidden="true" />
+            </button>
+          </div>
         </div>
-        <div className="flex">
-          <button className="p-2 px-3 text-xs hover:bg-gray-50">
-            <i className="ms-Icon ms-Icon--ChromeMinimize" aria-hidden="true" />
+        <div className="border-b">
+          <button className="px-3 py-0.5 text-sm bg-blue-700 text-white">
+            File
           </button>
-          <button className="p-2 px-3 text-xs hover:bg-gray-50">
-            <i className="ms-Icon ms-Icon--Checkbox" aria-hidden="true" />
-          </button>
-          <button className="p-2 px-3 text-xs hover:bg-gray-50">
-            <i className="ms-Icon ms-Icon--ChromeClose" aria-hidden="true" />
-          </button>
+          <button className="px-3 py-0.5 text-sm">Share</button>
+          <button className="px-3 py-0.5 text-sm">View</button>
+        </div>
+        <div className="px-3 flex py-4 space-x-4">
+          <div className="w-2/12 text-xs">
+            <ExplorerPaneItem iconName="Recent" text="Quick access" />
+            <ExplorerPaneItem iconName="TVMonitor" text="Desktop" />
+            <ExplorerPaneItem iconName="Download" text="Downloads" />
+            <ExplorerPaneItem iconName="DocumentSet" text="Documents" />
+          </div>
+          <div className="w-10/12">
+            {["controllers", "node_modules", "middlewares", "routes"].map(
+              (folder) => (
+                <Folder name={folder} />
+              )
+            )}
+            {["global.d.ts", "index.ts"].map((file) => (
+              <TSFile name={file} />
+            ))}
+          </div>
         </div>
       </div>
-      <div className="border-b">
-        <button className="px-3 py-0.5 text-sm bg-blue-700 text-white">
-          File
-        </button>
-        <button className="px-3 py-0.5 text-sm">Share</button>
-        <button className="px-3 py-0.5 text-sm">View</button>
-      </div>
-      <div className="px-3 flex py-4 space-x-4">
-        <div className="w-2/12 text-xs">
-          <ExplorerPaneItem iconName="Recent" text="Quick access" />
-          <ExplorerPaneItem iconName="TVMonitor" text="Desktop" />
-          <ExplorerPaneItem iconName="Download" text="Downloads" />
-          <ExplorerPaneItem iconName="DocumentSet" text="Documents" />
-        </div>
-        <div className="w-10/12">
-          {["controllers", "node_modules", "middlewares", "routes"].map(
-            (folder) => (
-              <Folder name={folder} />
-            )
-          )}
-          {["global.d.ts", "index.ts"].map((file) => (
-            <TSFile name={file} />
-          ))}
-        </div>
-      </div>
-    </div>
+    </Draggable>
   );
 }
